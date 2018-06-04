ef62d915b0c4845728400da917ab936ced7e8a05
ZCLMK <zacharie.NTC@gmail.com>
2018-06-04 11:05:33 +0200

    lien turbolink ajoute a custom.js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,18 +1,16 @@
-$(function(){
-    // $(".logo").click(function(){
-    //   $(".section").toggle(200);
+
+// $(function(){   
     // });
+ 
+   /* remplacé par */   
 
-
+$(document).on ("turbolinks:load", function(){
+      $('.nav-toggle').click(function(e) {
+        $('.main-nav').toggleClass('is-open');
+        $('.hamburger').toggleClass('is-open');
+        $('body').toggleClass('fixed');
+        $('#menu-app-slide').toggleClass('slider');
+      });
+    })
     // hamburger menu toggle
-    $('.nav-toggle').click(function() {
-      $('.main-nav').toggleClass('is-open');
-      $('.hamburger').toggleClass('is-open');
-      $('#menu-app-slide').toggleClass('slider');
-    });
 
-  // $('#menu-app-slide').hide();
-
-
-
-  });
