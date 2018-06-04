8a5b8b8021a0842bd122cff7509aece12f0e0f39
geraldy1st <geraldy.leondas@gmail.com>
2018-06-04 17:03:41 +0200

    clean css

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,1 +1,9 @@
-
+$(document).on ("turbolinks:load", function(){
+  $('#nav-toggle').click(function(e) {
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+    $('body').toggleClass('fixed');
+    $('#menu-app-slide').toggleClass('slider');
+    console.log("test");
+  });
+})
