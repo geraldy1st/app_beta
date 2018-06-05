59b59736e9076a12b19812ad047b29ab3673991a
geraldy1st <geraldy.leondas@gmail.com>
2018-06-05 18:38:32 +0200

    page games

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :gamers
   root 'home#index'
+
+  get 'home/profil', to: 'home#profil'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
